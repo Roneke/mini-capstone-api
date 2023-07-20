190570cb2e7143226980fcec80df46ceb31ec905
RONEKE BROWNE <lilsaint900@hotmail.com>
2023-07-20 18:55:55 -0400

    added jbuilder, test and methods updated

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,33 @@
 class OrdersController < ApplicationController
   def create
+    product = Product.find_by(id: params[:product_id])
+    price = product.price
+    subtotal = price * params[:quantity].to_i
+    tax = 0.09 * subtotal
+    total = subtotal + tax
+
     @order = Order.create(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
       subtotal: params[:subtotal],
       tax: params[:tax],
       total: params[:total],
     )
-    render json: @order.as_json
+    if @order.valid?
+      render :show
+    else
+      render json: { errors: @order.errors.full_messages }, status: 422
+    end
   end
 
   def show
     @order = Order.find_by(id: params["id"])
-    render json: @order.as_json
+    render :show
   end
 
   def index
-    @order = Order.all
-    render json: @order.as_json
+    @order = current_user.orders
+    render :index
   end
 end
